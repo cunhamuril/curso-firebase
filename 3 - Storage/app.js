eefd0ab03d8664da5e9e33d05c26ea86f32b79e4
Murilo Cunha <murilo.sant@hotmail.com>
2020-06-25 19:26:28 -0300

    feat(storage): upload de arquivos

--- 3 - Storage/app.js
+++ 3 - Storage/app.js
@@ -1,19 +1,41 @@
 /**
  * Variáveis com referencias dos inputs
  */
-var fileInput = document.getElementById('file-input');
-var stringInput = document.getElementById('string-input');
+var fileInput = document.getElementById("file-input");
+var stringInput = document.getElementById("string-input");
+
+/**
+ * Referencia para o storage do firebase criando uma pasta com o nome de arquivos.
+ */
+var ref = firebase.storage().ref("arquivos").child("nome");
 
 /**
  * Metodo que observa mudanças no input de arquivo
  */
 fileInput.onchange = function (event) {
-    
-}
+  var arquivo = event.target.files[0];
+
+  /**
+   * .child(nome) - Acessar o caminho para inserir o arquivo.
+   * ,put(arquivo) - Vai inserir o arquivo
+   */
+  ref
+    .child("arquivo")
+    .put(arquivo)
+    .then((snapshot) => {
+      console.log("snapshot", snapshot);
+
+      /**
+       * .getDownloadURL() - Retornar a url para download/apresentação desse arquivo enviado.
+       */
+      ref
+        .child("arquivo")
+        .getDownloadURL()
+        .then((url) => console.log("string para download: ", url));
+    });
+};
 
 /**
  * Metodo que observa mudanças no input de string
  */
-stringInput.onchange = function (event) {
-
-}
+stringInput.onchange = function (event) {};
